3eb10e74e4c21e225bef36c0e816f39a1955fd99
Lucas Quinto <lucasquinto22@gmail.com>
2025-04-21 14:30:53 -0300

    Adding project basic structure

---
+++ src/main/java/dev/lucas/finaltial_helper/FinaltialHelperApplication.java
@@ -0,0 +1,13 @@
+package dev.lucas.finaltial_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltialHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaltialHelperApplication.class, args);
+	}
+
+}
